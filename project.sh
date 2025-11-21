6ab9b0cd3251031035e6a568a46e203aef0723a4
rapture-11 <agnihotri.rushil09@gmail.com>
2025-11-21 11:16:11 +0530

    Update project.sh

--- project.sh
+++ project.sh
@@ -12,9 +12,9 @@ search_by_name() {
     result=$(find "$dir" -type f -name "$name" 2>/dev/null)
     if [ -n "$result" ]; then
         echo "$result"
-        echo "✅ Found!"
+        echo " Found!"
     else
-        echo "❌ Not Found!"
+        echo " Not Found!"
     fi
 }
 
@@ -26,9 +26,9 @@ search_by_extension() {
     result=$(find "$dir" -type f -name "*.$ext" 2>/dev/null)
     if [ -n "$result" ]; then
         echo "$result"
-        echo "✅ Found!"
+        echo "Found!"
     else
-        echo "❌ Not Found!"
+        echo "Not Found!"
     fi
 }
 
@@ -46,9 +46,9 @@ search_by_size() {
     fi
     if [ -n "$result" ]; then
         echo "$result"
-        echo "✅ Found!"
+        echo "Found!"
     else
-        echo "❌ Not Found!"
+        echo "Not Found!"
     fi
 }
 
